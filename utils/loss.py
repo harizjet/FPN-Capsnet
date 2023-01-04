897db4105534dfac06015d8e27894dcefb9b5d5e
Hariz Izzat <harizizzat09@gmail.com>
2023-01-04 22:15:33 +0800

    added loss fx

---
+++ utils/loss.py
@@ -0,0 +1,45 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+
+class CapsuleLoss(nn.Module):
+    """
+    Custom loss for capsule
+    """
+
+    def __init__(self):
+        super(CapsuleLoss, self).__init__()
+    
+    def __repr__(self):
+        return "CapsuleLoss"
+    
+    def forward(self, y, ypred):
+        labels = torch.eye(10).index_select(dim=0, index=y)
+
+        left = F.relu(0.9 - ypred, inplace=True) ** 2
+        right = F.relu(ypred - 0.1, inplace=True) ** 2
+
+        margin_loss = labels * left + 0.5 * (1. - labels) * right
+        margin_loss = margin_loss.sum()
+
+        return margin_loss
+
+
+class CrossEntropyLoss(nn.Module):
+    """
+    Cross Entropy Loss
+    """
+
+    def __init__(self):
+        super(CrossEntropyLoss, self).__init__()
+        self.critique = nn.CrossEntropyLoss()
+    
+    def __repr__(self):
+        return "CrossEntropyLoss"
+    
+    def forward(self, y, ypred):
+        ypred = torch.max(ypred, dim=-1).values
+
+        return self.critique(y.float(), ypred)
+        
