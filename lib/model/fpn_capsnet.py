96370c5a6857c7d4319800b83061410c484f6451
Hariz Izzat <harizizzat09@gmail.com>
2023-01-22 00:45:54 +0800

    find best hyperparameter for fpn-capsnet

--- lib/model/fpn_capsnet.py
+++ lib/model/fpn_capsnet.py
@@ -1,6 +1,3 @@
-'''
-Todo: Tweak hyperparameter to bridging between FPN and Capsnet
-'''
 import torch
 import torch.nn.functional as F
 from torch import nn
@@ -9,7 +6,7 @@ from lib.model.capsule import CapsuleLayer
 
 
 class Bottleneck(nn.Module):
-    expansion = 1
+    expansion = 2
 
     def __init__(self, in_planes, planes, stride=1):
         super(Bottleneck, self).__init__()
@@ -46,42 +43,42 @@ class FPNCapsuleNet(nn.Module):
 
         self.device = device
 
-        self.in_planes = 256
+        self.in_planes = 64
 
-        self.conv1 = nn.Conv2d(1, 256, kernel_size=9, stride=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(256)
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=9, stride=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
 
         # Bottom-up layers
-        self.layer1 = self._make_layer(block,  256, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 256, num_blocks[1], stride=1)
+        self.layer1 = self._make_layer(block,  64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=1)
 
         # Top layer
-        self.toplayer = nn.Conv2d(256, 256, kernel_size=1, stride=1, padding=0)  # Reduce channels
+        self.toplayer = nn.Conv2d(256, 32, kernel_size=1, stride=1, padding=0)  # Reduce channels
 
         # Smooth layers
-        self.smooth1 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
-        self.smooth2 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+        self.smooth1 = nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1)
+        self.smooth2 = nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1)
 
         # Lateral layers
-        self.latlayer1 = nn.Conv2d(256, 256, kernel_size=1, stride=1, padding=0)
-        self.latlayer2 = nn.Conv2d(256, 256, kernel_size=1, stride=1, padding=0)
+        self.latlayer1 = nn.Conv2d(128, 32, kernel_size=1, stride=1, padding=0)
+        self.latlayer2 = nn.Conv2d(64, 32, kernel_size=1, stride=1, padding=0)
 
         # Capsule for pipe1
-        self.primary_capsules1 = CapsuleLayer(num_capsules=8, num_route_nodes=-1, in_channels=256, out_channels=32,
-                                             kernel_size=9, stride=2, device=self.device)
-        self.digit_capsules1 = CapsuleLayer(num_capsules=10, num_route_nodes=32 * 6 * 6, in_channels=8,
+        self.primary_capsules1 = CapsuleLayer(num_capsules=8, num_route_nodes=-1, in_channels=32, out_channels=32,
+                                             kernel_size=3, stride=2, device=self.device)
+        self.digit_capsules1 = CapsuleLayer(num_capsules=10, num_route_nodes=512, in_channels=8,
                                            out_channels=16, device=self.device)
 
         # Capsule for pipe2
-        self.primary_capsules2 = CapsuleLayer(num_capsules=8, num_route_nodes=-1, in_channels=256, out_channels=32,
-                                             kernel_size=9, stride=2, device=self.device)
-        self.digit_capsules2 = CapsuleLayer(num_capsules=10, num_route_nodes=32 * 6 * 6, in_channels=8,
+        self.primary_capsules2 = CapsuleLayer(num_capsules=8, num_route_nodes=-1, in_channels=32, out_channels=32,
+                                             kernel_size=5, stride=2, device=self.device)
+        self.digit_capsules2 = CapsuleLayer(num_capsules=10, num_route_nodes=288, in_channels=8,
                                            out_channels=16, device=self.device)
 
         # Capsule for pipe3
-        self.primary_capsules3 = CapsuleLayer(num_capsules=8, num_route_nodes=-1, in_channels=256, out_channels=32,
+        self.primary_capsules3 = CapsuleLayer(num_capsules=8, num_route_nodes=-1, in_channels=32, out_channels=32,
                                              kernel_size=9, stride=2, device=self.device)
-        self.digit_capsules3 = CapsuleLayer(num_capsules=10, num_route_nodes=32 * 6 * 6, in_channels=8,
+        self.digit_capsules3 = CapsuleLayer(num_capsules=10, num_route_nodes=32, in_channels=8,
                                            out_channels=16, device=self.device)
 
     def _make_layer(self, block, planes, num_blocks, stride):
@@ -98,8 +95,8 @@ class FPNCapsuleNet(nn.Module):
 
     def _get_feature(self, x):
         # Bottom-up
-        c1 = F.relu(self.bn1(self.conv1(x)))
-        # c1 = F.max_pool2d(c1, kernel_size=3, stride=2, padding=1)
+        c1 = F.relu(self.conv1(x))
+        c1 = F.max_pool2d(c1, kernel_size=3, stride=2, padding=1)
         c2 = self.layer1(c1)
         c3 = self.layer2(c2)
         # Top-down
@@ -115,11 +112,15 @@ class FPNCapsuleNet(nn.Module):
     def forward(self, x):
         p1, p2, p3 = self._get_feature(x)
 
+        p1 = nn.BatchNorm2d(p1.size()[1]).to(self.device)(p1)
+        p2 = nn.BatchNorm2d(p2.size()[1]).to(self.device)(p2)
+        p3 = nn.BatchNorm2d(p3.size()[1]).to(self.device)(p3)
+
         x1 = F.relu(p1, inplace=True)
         x1 = self.primary_capsules1(x1)
         x1 = self.digit_capsules1(x1).squeeze(2).squeeze(2).transpose(0, 1)
         logits1 = (x1 ** 2).sum(dim=-1) ** 0.5
-
+        
         x2 = F.relu(p2, inplace=True)
         x2 = self.primary_capsules2(x2)
         x2 = self.digit_capsules2(x2).squeeze(2).squeeze(2).transpose(0, 1)
@@ -129,7 +130,6 @@ class FPNCapsuleNet(nn.Module):
         x3 = self.primary_capsules3(x3)
         x3 = self.digit_capsules3(x3).squeeze(2).squeeze(2).transpose(0, 1)
         logits3 = (x3 ** 2).sum(dim=-1) ** 0.5
-
         logits = torch.stack((logits1, logits2, logits3), dim=1)
         logits = torch.sum(logits, 1)
 
@@ -143,4 +143,4 @@ def FPN101CapsuleNet(device=torch.device('cpu')):
     $ net = FPN101CapsuleNet()
     $ output = net(Variable(torch.randn(1, 1, 28, 28)))
     """
-    return FPNCapsuleNet(Bottleneck, [2,2,], device=device)
+    return FPNCapsuleNet(Bottleneck, [2,2], device=device)
