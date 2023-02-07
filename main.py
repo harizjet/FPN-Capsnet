4026c7cc34c86a81ea06f19c83fa807141db0a0c
Hariz Izzat <harizizzat09@gmail.com>
2023-02-07 16:35:44 +0800

    complete main.py

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
-import argparse
-from utils.engine import Engine, TestEngine
-import torch
+from mparser import argParser
+
 from lib.model.ipyramid_capsnet import IPyramidCapsuleNet
 from lib.model.fpn_capsnet import FPN101CapsuleNet
+
 from utils.loss import CapsuleLoss, CrossEntropyLoss
-from torch.optim import Adam
+from utils.engine import Engine, TestEngine
 from utils.dataset import MNISTdataset
 from utils.transform import *
+
+import torch
 from torchvision import transforms
+from torch.optim import Adam
 
 
-argParser = argparse.ArgumentParser()
-argParser.add_argument('--model', help='fpn-capsnet or ipyramid-capsnet', choices=['fpn-capsnet', 'ipyramid-capsnet'])
-argParser.add_argument('--device', help='cuda or cpu', choices=['cuda', 'cpu'])
-argParser.add_argument('--criterion', help='capsule-loss or cross-entropy', choices=['capsule-loss', 'cross-entropy'])
-argParser.add_argument('--lr_scheduler', help='exponential, step or none', choices=['exponential', 'step', 'none'])
-argParser.add_argument('--lr_decay', help='decay rate for lr (used by lr_scheduler)', type=float, default=1)
-argParser.add_argument('--lr_decay_step', help='number of epochs before each decay rate for lr (used by step lr_scheduler)', type=float, default=8)
-argParser.add_argument('--epochs', help='number of epochs to train', type=int, default=100)
-argParser.add_argument('--lr', help='learning rate for optimizer', type=float, default=0.00001)
-argParser.add_argument('--batch_size', help='batch size fo train', type=int, default=32)
-
+"""
+Example:
+python main.py \
+    --model fpn-capsnet \
+    --device cuda \
+    --criterion capsule-loss \
+    --lr_scheduler  step \
+    --lr_decay 0.98 \
+    --lr_decay_step 8 \
+    --epochs 1 \
+    --lr 0.00001 \
+    --batch_size 32
+"""
 
 if __name__ == "__main__":
     arg = argParser.parse_args()
@@ -34,7 +39,9 @@ if __name__ == "__main__":
     EPOCHS = arg.epochs
     LEARNING_RATE = arg.lr
     BATCHSIZE = arg.batch_size
-    BASE_DIR = "./src/model/"
+    MODEL_DIR = "./src/model/"
+    TEST_DIR = "./src/output/"
+    DATA_DIR = "./data/mnist/"
 
     match MODEL_NAME:
         case "fpn-capsnet":
@@ -59,17 +66,16 @@ if __name__ == "__main__":
             LR_SCHEDULER = None
 
     # full-train
-    transform_x = transforms.Compose([toTensor(), augmentation((28, 28), isBatch=True)])
-    transform_y = transforms.Compose([toTensor()])
-    DATASET = MNISTdataset(isTrain=True, transform_x=transform_x, transform_y=transform_y)
-    engine = Engine(basedir=BASE_DIR, model=MODEL, epochs=EPOCHS, batch_size=BATCHSIZE, 
+    transform_x = transforms.Compose([toTensor(type=torch.float32), augmentation((28, 28), isBatch=True)])
+    transform_y = transforms.Compose([toTensor(type=torch.int32)])
+    DATASET = MNISTdataset(isTrain=True, transform_x=transform_x, transform_y=transform_y, data_dir=DATA_DIR)
+    engine = Engine(basedir=MODEL_DIR, model=MODEL, epochs=EPOCHS, batch_size=BATCHSIZE, 
                     learning_rate=LEARNING_RATE, optimizer=OPTIMIZER, lr_scheduler=LR_SCHEDULER, 
                     criterion=CRITERION, train_dataset=DATASET, device=DEVICE)
     engine.train(valid=False)
     
     # full-test
-    transform_x = transforms.Compose([toTensor()])
-    transform_y = transforms.Compose([toTensor()])
-    DATASET = MNISTdataset(isTrain=False, transform_x=transform_x, transform_y=transform_y)
-    engine = TestEngine(basedir=BASE_DIR, model=MODEL, dataset=DATASET, device=DEVICE, model_name=engine.foldername)
+    transform_x = transforms.Compose([toTensor(type=torch.float32)])
+    DATASET = MNISTdataset(isTrain=False, transform_x=transform_x, data_dir=DATA_DIR)
+    engine = TestEngine(basedir=TEST_DIR, model=MODEL, dataset=DATASET, device=DEVICE, model_name=engine.foldername)
     engine.test()
